65a886880f4673350da187ba126e3be7067494da
Vit <mayllby@gmail.com>
2023-10-03 23:02:47 +0300

    добавил метод bool - задача 4

--- Seminars/Sem2/Program.cs
+++ Seminars/Sem2/Program.cs
@@ -126,7 +126,8 @@
 // Reduction ();
 
 
-// Задача 4
+// Задача 4 
+// (метод "void")
 // Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
 
 // 14 -> нет
@@ -152,3 +153,22 @@
 // }
 // Multiplicity ();
 
+
+// Задача 4 
+// (метод "bool")
+
+// bool Multiplicity(int num)
+// {
+//     if (num % 7 == 0 && num % 23 == 0)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+// System.Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// bool result = Multiplicity(num);
+// System.Console.WriteLine(result);
