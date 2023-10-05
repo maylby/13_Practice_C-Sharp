e18252effceb92d824f65517c502e6e29d555e7a
Vit <mayllby@gmail.com>
2023-10-05 10:01:35 +0300

    добавил решение задачи 15

--- Homework/Hom2/Program.cs
+++ Homework/Hom2/Program.cs
@@ -69,7 +69,50 @@
 // }
 
 // Задача 15: 
-// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+// int Prompt (string message)
+// {
+//     System.Console.Write(message);
+//     string value = Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
+
+// bool IsWeekend(int weekDay)
+// {
+//     if (weekDay > 5)
+//     {
+//         return true;
+        
+//     }
+//     return false;
+// }
+
+// bool ValidateWeekend(int number)
+// {
+//     if (number > 0 && number <= 7)
+//     {
+//         return true;
+        
+//     }
+//     Console.WriteLine("Это не день недели!");
+//     return false;
+// }
+
+// int weekDay = Prompt("Введите день недели: ");
+// if (ValidateWeekend(weekDay))
+// {
+//     if (IsWeekend(weekDay))
+//     {
+//         Console.WriteLine("Выходной, ура!");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Сегодня работаем!");
+//     }
+// }
