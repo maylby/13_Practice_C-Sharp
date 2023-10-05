4324b955595967e777de7c9ca4c022e4b8a708c8
Vit <mayllby@gmail.com>
2023-10-05 13:24:01 +0300

    добавил решение задачи 1

--- Seminars/Sem3/Program.cs
+++ Seminars/Sem3/Program.cs
@@ -1,1 +1,39 @@
 ﻿// Семинар 3 "Базовые алгоритмы" (продолжение)
+
+
+// 08:00
+// Задача 1
+// Напишите программу, которая, по номеру четверти, показывает 
+// диапазон возможных координат точек в данной четверти (x и y)
+
+void FindCoordinates(int quarter)
+{
+	if(quarter <= 4 && quarter >= 1)
+	{
+		if(quarter == 1)
+		{
+			Console.WriteLine("x+ y+");
+		}
+		if(quarter == 2)
+		{
+			Console.WriteLine("x- y+");
+		}
+		if(quarter == 3)
+		{
+			Console.WriteLine("x- y-");
+		}
+		if(quarter == 4)
+		{
+			Console.WriteLine("x+ y-");
+		}
+	}
+	else
+	{
+		Console.WriteLine("invalid input: ");
+	}
+}
+
+System.Console.Write("Input guarter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+FindCoordinates(num);
