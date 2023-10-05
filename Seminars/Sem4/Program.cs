eca36e1c9e3225cf267f9e5308809512c3f205b5
Vit <mayllby@gmail.com>
2023-10-05 20:12:01 +0300

    добавил задачу 1

--- Seminars/Sem4/Program.cs
+++ Seminars/Sem4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Семинар 4 "Функции"
+
+// 0:19:00
+// Задача 1
+// Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе
+
+int DigitCount (int num)
+{
+    int count = 1;
+    while (num > 10)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+System.Console.WriteLine("Imput number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(DigitCount(num));
