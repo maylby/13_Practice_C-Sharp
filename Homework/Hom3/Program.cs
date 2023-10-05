57ad8ccaa174d26558bd3c74c100f7549465a54f
Vit <mayllby@gmail.com>
2023-10-05 18:12:52 +0300

    добавил задачу 3

--- Homework/Hom3/Program.cs
+++ Homework/Hom3/Program.cs
@@ -64,43 +64,94 @@
 // A (3,6,8); B (2,1,-7) -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+
+// using System;
+
+// public class Answer
+// {
+//     private static double Length(int[] pointA, int[] pointB)
+//     {
+//       // Введите свое решение ниже
+
+      
+
+//     }
+
+//   // Не удаляйте и не меняйте метод Main! 
+//     public static void Main(string[] args) 
+//     {
+//         int x1, x2, x3, y1, y2, y3;
+
+//         if (args.Length >= 6) {
+//             x1 = int.Parse(args[0]);
+//             x2 = int.Parse(args[1]);
+//             x3 = int.Parse(args[2]);
+//             y1 = int.Parse(args[3]);
+//             y2 = int.Parse(args[4]);
+//             y3 = int.Parse(args[5]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             x1 = 3;
+//             x2 = 6;
+//             x3 = 8;
+//             y1 = 2;
+//             y2 = 1;
+//             y3 = -7;
+//         }
+
+//         // Не удаляйте строки ниже
+//         double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+//         Console.WriteLine($"{result:F2}");
+//     }
+// }
+
+
+// Таблица кубов
+
+// Инструкция по использованию платформы
+
+// Внутри класса Answer напишите метод ShowCube, 
+// который принимает на вход число (N) 
+// и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+
+// N = 3 
+// /*
+// 1
+// 8
+// 27
+// */
+
+// N = 5
+// /*
+// 1
+// 8
+// 27
+// 64
+// 125
+// */
+
 using System;
 
 public class Answer
 {
-    private static double Length(int[] pointA, int[] pointB)
+   static void ShowCube(int N)
     {
       // Введите свое решение ниже
-
-
+        
     }
 
   // Не удаляйте и не меняйте метод Main! 
-    public static void Main(string[] args) 
-    {
-        int x1, x2, x3, y1, y2, y3;
+      static public void Main(string[] args) {
+        int N;
 
-        if (args.Length >= 6) {
-            x1 = int.Parse(args[0]);
-            x2 = int.Parse(args[1]);
-            x3 = int.Parse(args[2]);
-            y1 = int.Parse(args[3]);
-            y2 = int.Parse(args[4]);
-            y3 = int.Parse(args[5]);
+        if (args.Length >= 1) {
+            N = int.Parse(args[0]);
         } else {
            // Здесь вы можете поменять значения для отправки кода на Выполнение
-            x1 = 3;
-            x2 = 6;
-            x3 = 8;
-            y1 = 2;
-            y2 = 1;
-            y3 = -7;
+            N = 6;
         }
 
         // Не удаляйте строки ниже
-        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
-        Console.WriteLine($"{result:F2}");
+        ShowCube(N);
     }
 }
-
-
