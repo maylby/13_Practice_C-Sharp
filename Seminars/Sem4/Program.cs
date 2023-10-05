68a60aa540218ff8f22acd2b47b3ca44374bfe1d
Vit <mayllby@gmail.com>
2023-10-05 22:55:29 +0300

    Добавил решение задачи 3

--- Seminars/Sem4/Program.cs
+++ Seminars/Sem4/Program.cs
@@ -57,3 +57,30 @@
 // // System.Console.WriteLine(CreateRandomArray(size, min, max)); // Данная конструкция тут не работает
 // int[] myArray = CreateRandomArray(size, min, max);
 // PrintArray(myArray);
+
+
+// 00:58:00
+// Студенты
+
+// Задача 3
+// Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// int Number (int A)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= A; i++)
+//     {
+//         sum = sum += i;
+//     }
+//     return sum;
+// }
+
+// System.Console.Write("Input A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(Number (A));
+
