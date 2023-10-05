c4241756d21abc1b768bd3b6dec068fb1a14c46f
Vit <mayllby@gmail.com>
2023-10-05 18:51:12 +0300

    добавил решение задачи 3

--- Homework/Hom3/Program.cs
+++ Homework/Hom3/Program.cs
@@ -114,44 +114,33 @@
 // который принимает на вход число (N) 
 // и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
 
-// N = 3 
-// /*
-// 1
-// 8
-// 27
-// */
+// using System;
 
-// N = 5
-// /*
-// 1
-// 8
-// 27
-// 64
-// 125
-// */
+// public class Answer
+// {
+//    static void ShowCube(int N)
+//     {
+//       // Введите свое решение ниже
+//         int index = 1;
+// 	    while (index <= N)
+// 	    {
+// 		    System.Console.WriteLine($"{index * index * index} ");
+// 		    index++;
+// 	    } 
+//     }
 
-using System;
+//   // Не удаляйте и не меняйте метод Main! 
+//       static public void Main(string[] args) {
+//         int N;
 
-public class Answer
-{
-   static void ShowCube(int N)
-    {
-      // Введите свое решение ниже
-        
-    }
+//         if (args.Length >= 1) {
+//             N = int.Parse(args[0]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             N = 5;
+//         }
 
-  // Не удаляйте и не меняйте метод Main! 
-      static public void Main(string[] args) {
-        int N;
-
-        if (args.Length >= 1) {
-            N = int.Parse(args[0]);
-        } else {
-           // Здесь вы можете поменять значения для отправки кода на Выполнение
-            N = 6;
-        }
-
-        // Не удаляйте строки ниже
-        ShowCube(N);
-    }
-}
+//         // Не удаляйте строки ниже
+//         ShowCube(N);
+//     }
+// }
