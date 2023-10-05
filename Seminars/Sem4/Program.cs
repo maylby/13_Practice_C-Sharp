fc2a56ec1189258fcf215dd213885f1d063069f9
Vit <mayllby@gmail.com>
2023-10-05 20:51:05 +0300

    добавил условие задачи 2

--- Seminars/Sem4/Program.cs
+++ Seminars/Sem4/Program.cs
@@ -5,17 +5,23 @@
 // Напишите программу, которая принимает на вход число
 // и выдаёт количество цифр в числе
 
-int DigitCount (int num)
-{
-    int count = 1;
-    while (Math.Abs(num) > 10) // "(Math.Abs(num)" - число по модулю (положительное и отрицательное)
-    {
-        num = num / 10;
-        count++;
-    }
-    return count;
-}
-System.Console.WriteLine("Imput number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// int DigitCount (int num)
+// {
+//     int count = 1;
+//     while (Math.Abs(num) > 10) // "(Math.Abs(num)" - число по модулю (положительное и отрицательное)
+//     {
+//         num = num / 10;
+//         count++;
+//     }
+//     return count;
+// }
+// System.Console.WriteLine("Imput number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine(DigitCount(num));
+// System.Console.WriteLine(DigitCount(num));
+
+// Архип Чижанов 
+// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
