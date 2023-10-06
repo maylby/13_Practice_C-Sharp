2de4564221817424c7fb2b5aa1b88a9908ea31c9
Vit <mayllby@gmail.com>
2023-10-06 09:55:57 +0300

    Добавил условие задачи 4

--- Seminars/Sem5/Program.cs
+++ Seminars/Sem5/Program.cs
@@ -12,57 +12,64 @@
 // сумма положительных чисел равна 29, 
 // сумма отрицательных равна -20.
 
-int[] CreateRandomArray(int size, int minValue, int maxValue) 
-{
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next(minValue, maxValue +1);
-    }
-    return array;
-}
+// int[] CreateRandomArray(int size, int minValue, int maxValue) 
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue +1);
+//     }
+//     return array;
+// }
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++ )
-    {
-        System.Console.Write( array[i] + " ");
-    }
-    System.Console.WriteLine(); // <- Пустая строка в блоке вывода на экран, для удобства чтения
-}
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++ )
+//     {
+//         System.Console.Write( array[i] + " ");
+//     }
+//     System.Console.WriteLine(); // <- Пустая строка в блоке вывода на экран, для удобства чтения
+// }
 
-void FindMinMaxSum(int[] array)
-{
-    int positivSum = 0;
-    int negativSum = 0;
-    for (int i = 0; i < array.Length; i++ )
-    {
-        if (array[i] > 0)
-        {
-            positivSum += array[i]; // <- Краткая запись: "positivSum = positivSum + arrau[i]"
-        }
-        else
-        {
-            negativSum += array[i];
-        }
-    }
-    System.Console.WriteLine($"Sum of positivSum arrau elements -> {positivSum} \nSum of negativSum arrau elements -> {negativSum}");
+// void FindMinMaxSum(int[] array)
+// {
+//     int positivSum = 0;
+//     int negativSum = 0;
+//     for (int i = 0; i < array.Length; i++ )
+//     {
+//         if (array[i] > 0)
+//         {
+//             positivSum += array[i]; // <- Краткая запись: "positivSum = positivSum + arrau[i]"
+//         }
+//         else
+//         {
+//             negativSum += array[i];
+//         }
+//     }
+//     System.Console.WriteLine($"Sum of positivSum arrau elements -> {positivSum} \nSum of negativSum arrau elements -> {negativSum}");
 
-    // Значок "\n" разделяет две разных записи в одной строке, а
-    // значок "+" (плюс) связывает текст в единый блок, в случае переноса части текста на другую строку. 
-    // Выше стоящюю конструкцию не удалось разделить на части и перенести на другую строку описанным способом. 
-    // Можно записать, только отдельной строкой в конструкции "System.Console.WriteLine($"... -> {}");"
-    // Для примера оформления, оставил запись в одну строку.
-}
+//     // Значок "\n" разделяет две разных записи в одной строке, а
+//     // значок "+" (плюс) связывает текст в единый блок, в случае переноса части текста на другую строку. 
+//     // Выше стоящюю конструкцию не удалось разделить на части и перенести на другую строку описанным способом. 
+//     // Можно записать, только отдельной строкой в конструкции "System.Console.WriteLine($"... -> {}");"
+//     // Для примера оформления, оставил запись в одну строку.
+// }
 
-System.Console.WriteLine("Imput array size: ");
-int size = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Imput minimal value: ");
-int min = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Imput maxsimal value: ");
-int max = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Imput array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Imput minimal value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Imput maxsimal value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
 
-int[] myArray = CreateRandomArray(size, min, max);
-PrintArray(myArray);
-FindMinMaxSum(myArray);
+// int[] myArray = CreateRandomArray(size, min, max);
+// PrintArray(myArray);
+// FindMinMaxSum(myArray);
 
+
+// 00:25:30
+
+// Задача 4 (из презентации семинара).
+// Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива,
+// значения которых лежат в отрезке [10,99].
