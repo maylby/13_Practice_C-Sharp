9fd089526ca2f9500e045c2b78e415ee5392305a
Vit <mayllby@gmail.com>
2023-10-06 20:12:01 +0300

    Добавил решение задачи 2

--- Seminars/Sem5/Program.cs
+++ Seminars/Sem5/Program.cs
@@ -320,3 +320,48 @@
 // System.Console.WriteLine(ChangeElements(myArray1, myArray2));
 
 
+
+// Задача 2 
+// (Илья Канаев) 
+
+// Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue) 
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue +1);
+//     }
+//     return array;
+// }
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++ )
+//     {
+//         System.Console.Write( array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }    
+// int[] ChangeElements(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = array[i] * (-1);
+//     }
+//     return array;
+// }
+// System.Console.Write("Imput array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Imput minimal value: ");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Imput maximal value: ");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
+// PrintArray(myArray);
+// PrintArray(ChangeElements(myArray));
+
