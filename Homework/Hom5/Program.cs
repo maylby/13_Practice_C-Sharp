d07b888685d7f5b80396e199ee0813abe6751a1f
Vit <mayllby@gmail.com>
2023-10-08 17:19:04 +0300

    Добавил условие задачи 34

--- Homework/Hom5/Program.cs
+++ Homework/Hom5/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Практичечкая работа 5 по семинару "Функции и одномерные массивы"
+
+// Задача 34: 
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int EvenNumberCount(int[] count) //(???)
+{
+	int count = 0;
+    
+	for (int i = 0; i < array.Length; i++ )
+	{
+		if (array[i]%2 == 0);
+		{
+			count++	
+		}
+	}
+	retun count;
+}
