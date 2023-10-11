6b344987c25ba45feac9363e1427885c348267e4
Vit <mayllby@gmail.com>
2023-10-11 18:23:20 +0300

    Добавил условие задачи 27

--- Homework/Hom4-W/Program.cs
+++ Homework/Hom4-W/Program.cs
@@ -6,38 +6,50 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int Power(int powerBase, int exponent)
-{
-    int power = 1;
-    for (int i = 0; i < exponent; i++)
-    {
-        power *= powerBase; // power = power * powerBase
-    }
-    return power;
-}
+// int Power(int powerBase, int exponent)
+// {
+//     int power = 1;
+//     for (int i = 0; i < exponent; i++)
+//     {
+//         power *= powerBase; // power = power * powerBase
+//     }
+//     return power;
+// }
 
-bool ErrorExpo(int exponent)
-{
-	if (exponent < 0)
-	{
-		Console.WriteLine("Показатель степени не должен быть меньше нуля");
-		return false; 
-	}
-	return true;
-}
+// bool ErrorExpo(int exponent)
+// {
+// 	if (exponent < 0)
+// 	{
+// 		Console.WriteLine("Показатель степени не должен быть меньше нуля");
+// 		return false; 
+// 	}
+// 	return true;
+// }
 
-void PrintPower(int[] powerBase, int[] exponent, int[] power)
-{
-    for (int i = 0; i < exponent; i++ )
-    {
-        Console.Write($"{powerBase}, {exponent} -> {power}");
-    }
-}
+// void PrintPower(int[] powerBase, int[] exponent, int[] power)
+// {
+//     for (int i = 0; i < exponent; i++ )
+//     {
+//         Console.Write($"{powerBase}, {exponent} -> {power}");
+//     }
+// }
 
-System.Console.WriteLine("Введите значение основания: ");
-int powerBase = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите значение показателя: ");
-int exponent = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите значение основания: ");
+// int powerBase = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите значение показателя: ");
+// int exponent = Convert.ToInt32(Console.ReadLine());
 
-int[] myPower = Power(powerBase, exponent);
-PrintPower(myPower);
+// int[] myPower = Power(powerBase, exponent);
+// PrintPower(myPower);
+
+
+// Задача 27: 
+// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12 
+
+// int SumNumber(int num)
+
