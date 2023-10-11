1e6e83b77d572963242e56519fde4cca113ad072
Vit <mayllby@gmail.com>
2023-10-11 14:49:15 +0300

    Добавил задачу 25

--- Homework/Hom4-W/Program.cs
+++ Homework/Hom4-W/Program.cs
@@ -1,10 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// Домашнее задание по семинару 4 
+﻿// Домашнее задание по семинару 4 
 
 // Задача 25: 
-//Напишите цикл, который принимает на вход два числа (A и B) 
+// Напишите цикл, который принимает на вход два числа (A и B) 
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase; // power = power * powerBase
+    }
+    return power;
+}
+
+bool ErrorExpo(int exponent)
+{
+	if (exponent < 0)
+	{
+		Console.WriteLine("Показатель степени не должен быть меньше нуля");
+		return false; 
+	}
+	return true;
+}
+
+void PrintPower(int[] powerBase, int[] exponent, int[] power)
+{
+    for (int i = 0; i < exponent; i++ )
+    {
+        Console.Write($"{powerBase}, {exponent} -> {power}");
+    }
+}
+
+System.Console.WriteLine("Введите значение основания: ");
+int powerBase = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите значение показателя: ");
+int exponent = Convert.ToInt32(Console.ReadLine());
+
+int[] myPower = Power(powerBase, exponent);
+PrintPower(myPower);
--- Homework/Hom4/Program.cs
+++ Homework/Hom4/Program.cs
@@ -6,41 +6,41 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int PowerBase(int a, int b, int result)
-{
-	int result = 1;
-	for (int i = 0; i < b; i++)
-	{
-		result = result * a;
-	}
-	return result;
-}
+// int PowerBase(int a, int b, int result)
+// {
+// 	int power = 1;
+// 	for (int i = 0; i < b; i++)
+// 	{
+// 		power = power * a;
+// 	}
+// 	return result;
+// }
 
-bool ErrorExpo(int b)
-{
-	if (b < 0)
-	{
-		Console.WriteLine("Введите число больше нуля");
-		return false; 
-	}
-	return true;
-}
+// bool ErrorExpo(int b)
+// {
+// 	if (b < 0)
+// 	{
+// 		Console.WriteLine("Введите число больше нуля");
+// 		return false; 
+// 	}
+// 	return true;
+// }
 
-void PrintPower(int[] a, int[] b, int[] result)
-{
-    for (int i = 0; i < b; i++ )
-    {
-        System.Console.Write($"{a}, {b} -> {result}");
-    }
-}
+// void PrintPower(int[] a, int[] b, int[] result)
+// {
+//     for (int i = 0; i < b; i++ )
+//     {
+//         System.Console.Write($"{a}, {b} -> {result}");
+//     }
+// }
 
-System.Console.WriteLine("Введите основание: ");
-int a = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите експоненту: ");
-int в = Convert.ToInt32(Console.ReadLine());
+// // System.Console.WriteLine("Введите основание: ");
+// // int a = Convert.ToInt32(Console.ReadLine());
+// // System.Console.WriteLine("Введите експоненту: ");
+// // int в = Convert.ToInt32(Console.ReadLine());
 
-int[] myResult = PowerBase(a, b, result)
-PrintPower(myResult)
+// int[] myResult = PowerBase(a, b, result);
+// PrintPower(myResult);
 
 
 
