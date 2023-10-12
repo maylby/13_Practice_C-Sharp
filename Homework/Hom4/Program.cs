a74d64df91f4c4e0fc9f136b5ea5d02829199b48
Vit <mayllby@gmail.com>
2023-10-12 17:08:37 +0300

    Откорректировал код задачи 1

--- Homework/Hom4/Program.cs
+++ Homework/Hom4/Program.cs
@@ -151,39 +151,38 @@
 // int[] myArray = CreateRandomArray(size, min, max);
 // PrintArray(myArray);
 
- 
+
 
 // Задача 1 из семинара 4 (Архип Чижанов)
 // Напишите программу, которая принимает на вход число
 // и выдаёт количество цифр в числе
 
 
-// int[] CreateRandomArray(int size, int minValue, int maxValue)
-// {
-// int[] array = new int[size];
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
 
-// for (int i = 0; i < size; i++)
-// {
-// array[i] = new Random().Next(minValue, maxValue + 1);
-// }
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
 
-// return array;
-// }
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write( array[i] + " " );
+    }
+}
 
-// void PrintArray(int[] array)
-// {
-// for (int i = 0; i < array.Length; i++)
-// {
-// System.Console.Write( array[i] + " " );
-// }
-// }
+System.Console.WriteLine("Input array size: " );
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value: " );
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value: " );
+int max = Convert.ToInt32(Console.ReadLine());
 
-// System.Console.WriteLine("Input array size: " );
-// int size = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("Input minimal value: " );
-// int min = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("Input maximal value: " );
-// int max = Convert.ToInt32(Console.ReadLine());
-
-// int[] myArray = CreateRandomArray(size, min, max);
-// PrintArray(myArray);
+int[] myArray = CreateRandomArray(size, min, max);
+PrintArray(myArray);
