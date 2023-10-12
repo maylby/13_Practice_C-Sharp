74c44b53e7b752da6e8d02048bdb65bef39d2681
Vit <mayllby@gmail.com>
2023-10-12 23:38:39 +0300

    Добавил решение задачи 34

--- Homework/Hom5-W/Program.cs
+++ Homework/Hom5-W/Program.cs
@@ -5,42 +5,42 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int[] CreateRandomArray(int size) 
-{
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next();
-		if (array[i] > 99 && array[i] < 1000) return array;
-    }
-    return array;
-}
+// int[] CreateRandomArray(int size) 
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next();
+// 		if (array[i] > 99 && array[i] < 1000) return array;
+//     }
+//     return array;
+// }
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++ )
-    {
-        System.Console.Write(array + " ");
-    }
-    System.Console.WriteLine();
-}  
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++ )
+//     {
+//         System.Console.Write(array + " ");
+//     }
+//     System.Console.WriteLine();
+// }  
 
-int EvenNumberCount(int[] array) 
-{
-	int count = 0;
-	for (int i = 0; i < array.Length; i++ )
-	{
-		if (array[i]%2 == 0)
-		{
-			count++;	
-		}
-	}
-    return count;
-}
+// int EvenNumberCount(int[] array) 
+// {
+// 	int count = 0;
+// 	for (int i = 0; i < array.Length; i++ )
+// 	{
+// 		if (array[i]%2 == 0)
+// 		{
+// 			count++;	
+// 		}
+// 	}
+//     return count;
+// }
 
-System.Console.WriteLine("Imput array size: ");
-int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Imput array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
 
-int[] myArray = CreateRandomArray(size);
-PrintArray(myArray);
-System.Console.WriteLine($"{EvenNumberCount(myArray)}");
+// int[] myArray = CreateRandomArray(size);
+// PrintArray(myArray);
+// System.Console.WriteLine($"{EvenNumberCount(myArray)}");
