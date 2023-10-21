16bd923458e10279da12756bc25928e5196234f1
Vit <mayllby@gmail.com>
2023-10-21 15:47:23 +0300

    исправил ошибки в задаче 3

--- Seminars/Sem5/Program.cs
+++ Seminars/Sem5/Program.cs
@@ -198,9 +198,9 @@
 // int[] myArray = CreateRandomArray(size, minValue, maxValue);
 // PrintArray(myArray);
 // // // System.Console.WriteLine(RequiredNumber(num, myArray));
-// // // RequiredNumber(num, myArray);
-// // // System.Console.WriteLine($"Yes, {num} -> in the array"); // разделить вывод ответов "No" и "Yes"
-// // // System.Console.WriteLine($"No {num} -> in the array");
+// // // // RequiredNumber(num, myArray);
+// // // // System.Console.WriteLine($"Yes, {num} -> in the array"); // разделить вывод ответов "No" и "Yes"
+// // // // System.Console.WriteLine($"No {num} -> in the array");
 // System.Console.WriteLine(ExistenceInArray(num, myArray));
 
 
@@ -253,68 +253,11 @@
 
 
 // 00:57:50
-
-// Задача 2. 
-// Напишите программу замена элементов массива: 
-// положительные элементы замените на соответствующие отрицательные, и наоборот.
-
-//[-4, -8, 8, 2] -> [4, 8, -8, -2]
-
-
-// int[] CreateRandomArray(int size, int[] array1, int array2) 
-// {
-//     int[] array = new int[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         array[i] = new Random().Next(minValue, maxValue +1);
-//     }
-//     return array;
-// }
-
-// void PrintArray(int[] array1, int[] array2)
-// {
-//     for (int i = 0; i < array1.Length; i++ )
-//     {
-//         System.Console.Write( array1[i] + " ");
-//     }
-//         for (int i = 0; i < array2.Length; i++ )
-//     {
-//         System.Console.Write( array2[i] + " ");
-//     }
-//     System.Console.WriteLine(); // <- Пропуск в блоке вывода результата на экран
-// }
-
-// // Начало здесь
-
-// int[] ChangeElements(int[] array1, int[] array2)
-// {
-//     // int positiv = 0;
-//     // int negativ = 0;
-//     for (int i = 0; i < array1.Length; i++)
-//     {
-//         if (array1[i] < 0) return array2;
-//     }
-//     return array1;
-// }
-
-// System.Console.Write("Imput array size: ");
-// int size = Convert.ToInt32(Console.ReadLine());
-// System.Console.Write("Imput number: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-
-// int[] myArray1 = CreateUserArray(size);
-// int[] myArray2 = CreateUserArray(size);
-// PrintArray(myArray1, myArray2);
-// System.Console.WriteLine(ChangeElements(myArray1, myArray2));
-
-
-
-// Задача 2 
 // (Илья Канаев) 
 
+// Задача 2 
 // Напишите программу замена элементов массива: 
 // положительные элементы замените на соответствующие отрицательные, и наоборот.
-
 //[-4, -8, 8, 2] -> [4, 8, -8, -2]
 
 // int[] CreateRandomArray(int size, int minValue, int maxValue) 
@@ -338,10 +281,11 @@
 // {
 //     for (int i = 0; i < array.Length; i++)
 //     {
-//         array[i] = array[i] * (-1);
+//         array[i] *= -1; // array[i] = array[i] * (-1);
 //     }
 //     return array;
 // }
+
 // System.Console.Write("Imput array size: ");
 // int size = Convert.ToInt32(Console.ReadLine());
 // System.Console.Write("Imput minimal value: ");
