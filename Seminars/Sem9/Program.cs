c5893e706728b6e0fb9a11a9040a12266d935b8b
Vit <mayllby@gmail.com>
2023-10-29 15:32:17 +0300

    Добавил задачу 2

--- Seminars/Sem9/Program.cs
+++ Seminars/Sem9/Program.cs
@@ -1,5 +1,6 @@
 ﻿// 13_(С-09) 
 // Семинар 9. Рекурсия
+// https://gb.ru/lessons/362986
 
 // Задача 1.
 // Задать число N. Написать программу, выводящую все натуральные числа от 1 до N.
@@ -19,3 +20,20 @@
 // int n = Convert.ToInt32(Console.ReadLine());
 
 // ShowNumbers(n);
+
+
+// Задача 2.
+// Программа принимает на вход число и возвращает сумму его цифр.
+
+// int FindSumDigit(int num)
+// {
+//     if(num > 0) 
+//     {
+//         return FindSumDigit(num / 10) + num % 10;
+//     }
+//     else return num;
+// }
+// Console.Write("Input N: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write($"N = {num} -> sum digit: {FindSumDigit(num)}");
