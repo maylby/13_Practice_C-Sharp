dfaf1ff080cc431ce19849addb3c74c98f58c5b1
Vit <mayllby@gmail.com>
2023-10-29 19:17:34 +0300

    Закрыл сессию

--- Homework/Hom9/Program.cs
+++ Homework/Hom9/Program.cs
@@ -4,9 +4,10 @@
 
 
 // Задача 64: 
-// Задайте значение N. 
-// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Задать значение N. Написать программу, которая 
+// выводит все натуральные числа в промежутке от N до 1. 
 // Выполнить с помощью рекурсии.
+
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
@@ -14,19 +15,23 @@
 // {
 // 	if (n >= 1)
 // 	{
-// 		Console.Write($"{n}, "); // смена мест "CW" и метода для обратного порядка вывода чисел
+// 		Console.Write($"{n}, "); 
 // 		ShowReNumbers(n - 1);
 // 	}
 // }
 
 // Console.Write("Input N: ");
 // int n = Convert.ToInt32(Console.ReadLine());
-// ShowReNumbers(n);
+
+// Console.Write($"N = {n} -> "); ShowReNumbers(n);
+
 
 
 // Задача 66: 
 // Задайте значения M и N. 
-// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
@@ -55,3 +60,35 @@
 
 // int result = FindSumNumbers(n, m);
 // Console.Write($"M = {m}; N = {n} -> {result}");
+
+
+
+// Задача 68: 
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// int Ackerman(int n, int m)
+// {
+//     if (n == 0) return m + 1;
+//     else
+//     {
+//         if((n != 0) && (m == 0)) 
+//         {
+//             return Ackerman(n - 1, 1);
+//         } 
+//         else 
+//         {
+//             return Ackerman(n - 1, Ackerman(n, m - 1));
+//         }  
+//     }
+// }
+
+// Console.Write("Input N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// int result = Ackerman(n, m);
+// System.Console.WriteLine($"m = {m}, n = {n} -> Ackerman({m}, {n}) = {result}");
