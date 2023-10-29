0bed10efc6a87800bc942c0dbf7e30c1106661ec
Vit <mayllby@gmail.com>
2023-10-29 14:54:48 +0300

    Добавил задачу 1

--- Seminars/Sem9/Program.cs
+++ Seminars/Sem9/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 13_(С-09) 
+// Семинар 9. Рекурсия
+
+// Задача 1.
+// Задать число N. Написать программу, выводящую все натуральные числа от 1 до N.
+// N = 5 -> 1, 2, 3, 4, 5
+// N = 6 -> 1, 2, 3, 4, 5, 6
+
+// void ShowNumbers(int n)
+// {
+//     if(n >= 1)
+//     {
+//         ShowNumbers(n - 1);
+//         System.Console.Write(n + " ");
+//     }
+// }
+
+// Console.Write("Input N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// ShowNumbers(n);
